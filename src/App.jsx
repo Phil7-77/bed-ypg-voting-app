73e8083adb43116a6545dbccefe80614c6bef9b6
Phil7-77 <philip@traveltide.uk>
2025-09-08 22:04:45 +0000

    fix: Set correct production API URL

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useState, useMemo } from 'react';
 
 // --- Configuration ---
 // THIS IS THE MOST IMPORTANT STEP.
-
-const API_URL = "https://script.google.com/macros/s/AKfycbzIxaCRHFAifcHBmF_vZz5wmLBSXxPx1Qh4V8HKAgmKMkMOOFSXpfUuljlAHEsV9TJz/exec";
+// You MUST replace this with the new URL you get after deploying the backend script.
+const API_URL = "https://script.google.com/macros/s/AKfycbxG8U5l0ndhFDUrXFWB82GvGoWDWWCyLvXSaiZxicCCl8YXet1fJKLxpR12cI51rMH16w/exec";
 
 // --- SVG Icons ---
 const BackArrowIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" /></svg>);
@@ -237,7 +237,6 @@ const ConfirmationModal = ({ isOpen, handleGoToAuth }) => {
     </div>
   );
 };
-
 const AdminLoginPage = ({ handleAdminLogin, isLoading, resetToHome }) => {
   const [adminId, setAdminId] = useState('');
   const [password, setPassword] = useState('');
